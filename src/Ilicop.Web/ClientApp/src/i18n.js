11d92319232c99579e4f7e59acf3633e19f80937
Joel Wälti <joel.waelti@geowerkstatt.ch>
2025-10-28 16:46:58 +0100

    Add i18n with language selection dropdown

---
+++ src/Ilicop.Web/ClientApp/src/i18n.js
@@ -0,0 +1,35 @@
+import i18n from "i18next";
+import backend from "i18next-http-backend";
+import LanguageDetector from "i18next-browser-languagedetector";
+import { initReactI18next } from "react-i18next";
+
+export const SupportedLanguages = ["de", "fr"];
+
+i18n
+  .use(backend)
+  .use(initReactI18next)
+  .use(LanguageDetector)
+  .init({
+    detection: {
+      order: ["cookie", "navigator", "htmlTag"],
+      lookupCookie: "i18next",
+      caches: ["cookie"],
+    },
+    backend: {
+      loadPath: `/locale/{{lng}}/{{ns}}.json`,
+      allowMultiLoading: false,
+    },
+    react: {
+      useSuspense: false,
+    },
+    supportedLngs: SupportedLanguages,
+    ns: ["common"],
+    defaultNS: "common",
+    interpolation: {
+      escapeValue: false,
+      formatSeparator: ",",
+      transSupportBasicHtmlNodes: true,
+    },
+  });
+
+export default i18n;
